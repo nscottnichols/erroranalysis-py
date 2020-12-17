32f52c356cf07a16e4df69ebb461d824e790bc0e
Nathan Nichols <Nathan.Nichols@uvm.edu>
2020-12-16 21:51:00 -0500

    add examples and update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
-    packages=["graphenetools"],
+    packages=["erroranalysis"],
     include_package_data=True,
     install_requires=["numpy", "matplotlib"],
     entry_points={
